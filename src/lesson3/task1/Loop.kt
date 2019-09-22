4230058b81e262c57e245cf0e81165dbaad2913a
BozhkoAV <avbozhko@mail.ru>
2019-09-22 15:40:45 +0300

    Исправил, что смог в Уроке 2 и 3 Добавил тест в Уроке 3

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -73,7 +73,7 @@ fun digitNumber(n: Int): Int {
     do {
         count++
         number /= 10
-    } while (number > 0)
+    } while (abs(number) > 0)
     return count
 }
 
@@ -83,11 +83,18 @@ fun digitNumber(n: Int): Int {
  * Найти число Фибоначчи из ряда 1, 1, 2, 3, 5, 8, 13, 21, ... с номером n.
  * Ряд Фибоначчи определён следующим образом: fib(1) = 1, fib(2) = 1, fib(n+2) = fib(n) + fib(n+1)
  */
-fun fib(n: Int): Int =
-    when {
-        n < 3 -> 1
-        else -> fib(n - 1) + fib(n - 2)
+fun fib(n: Int): Int {
+    var a = 1
+    var b = 1
+    var i = 0
+    while (i < n - 2) {
+        val c = a + b
+        a = b
+        b = c
+        i++
     }
+    return b
+}
 
 /**
  * Простая
